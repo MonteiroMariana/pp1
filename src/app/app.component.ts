380a318a23053bc1929fd53ff77d5dc805e2c04f
Luis <luishenriquecorporativo@gmail.com>
2023-04-11 10:19:47 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pp1';
-}
+    n1:number=0;
+    n2:number = 0;
+    calcular:number=0
+    str1!:string;
+    str2!:string;
+
+calcular(){
+
+    if(this.str1 == this.str2){
+    alert("Selecione moeda diferentes!");
+  }
+   else if(this.str1 == "Real" && this.str2 =="Dolar"){
+    this.resp=this.n1*this.n2;
+   }
+   else if(this.str1 == "Real" && this.str2 == "Euro"){
+    this.resp=this.n1*this.n2;
+   }
+    else if(this.str1 == "Dolar" && this.str2 == "Real"){
+    this.resp=this.n1*this.n2;
+    }
+    else if(this.str1 == "Dolar" && this.str2 == "Euro"){
+    this.resp=this.n1*this.n2;
+    }
+    else if(this.str1 == "Euro" && this.str2=="Dolar"){
+    this.resp=this.n1*this.n2;
+    }
+    else if(this.str1 == "Euro" && this.str2=="Real"){
+    this.resp=this.n1*this.n2;
+    }
+    }
+    }
